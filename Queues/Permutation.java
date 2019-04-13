f8f838b195102a4bb52a6313a65b74fc079bbb5d
coops23 <lewiscooper2392@gmail.com>
2019-04-12 22:25:43 -0700

    First submission of assignment #2 scored 61. Needs work

---
+++ Queues/Permutation.java
@@ -0,0 +1,23 @@
+import edu.princeton.cs.algs4.StdIn;
+import java.util.Iterator;
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> deck = new RandomizedQueue<String>();
+        int count = Integer.parseInt(args[0]);
+
+        while (!StdIn.isEmpty()) {
+            String value = StdIn.readString();
+            deck.enqueue(value);
+        }
+
+        Iterator<String> iter  = deck.iterator();
+        for(int i = 0; i < count; i++)
+        {
+            if(iter.hasNext())
+            {
+                System.out.println(iter.next());
+            }
+        }
+    }
+}
--- Queues/RandomizedQueue.java
+++ Queues/RandomizedQueue.java
@@ -1,22 +1,18 @@
-import java.util.Iterator;
 import edu.princeton.cs.algs4.StdRandom;
+import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private Node first, last;
     private int count;
+    private Node first;
 
-    private class Node
-    {
+    private class Node {
         Item item;
         Node next;
-        Node prev;
     }
 
     public RandomizedQueue()                 // construct an empty randomized queue
     {
         count = 0;
-        first = null;
-        last = null;
     }
 
     public boolean isEmpty()                 // is the randomized queue empty?
@@ -31,58 +27,133 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     public void enqueue(Item item)           // add the item
     {
-        Node prevNode = last;
+        if(item == null) throw new java.lang.IllegalArgumentException();
 
-        last = new Node();
-        last.item = item;
-        last.prev = prevNode;
-        last.next = null;
+        //Basically inserting an element on the front
+        if (count == 0) {
+            Node newNode = new Node();
 
-        prevNode.next = last;
+            newNode.item = item;
+            newNode.next = null;
+            first = newNode;
+        }
+        else {
+            Node newNext = first;
+
+            first = new Node();
+            first.item = item;
+            first.next = newNext;
+        }
+
+        count++;
     }
 
     public Item dequeue()                    // remove and return a random item
     {
-        int pos = StdRandom.uniform(0, count - 1);
+        if(count == 0) throw new java.util.NoSuchElementException();
 
-        Iterator<Item> iterator  = this.iterator();
-        for(int i = 0; i < pos; i++)
-        {
-            iterator.next();
+        //Would normally just pop the front element but here we need to randomize things
+        int indexToRemove = StdRandom.uniform(0, count - 1);
+
+        int i = 0;
+        Node curr = first;
+        Node prev = curr;
+        while (i < indexToRemove) {
+            prev = curr;
+            curr = curr.next;
+            i++;
         }
 
-        Item item = iterator.next();
+        prev.next = curr.next;
+        count--;
+
+        return curr.item;
     }
 
     public Item sample()                     // return a random item (but do not remove it)
     {
+        if(count == 0) throw new java.util.NoSuchElementException();
 
+        int indexToGet = StdRandom.uniform(0, count - 1);
+
+        int i = 0;
+        Node curr = first;
+        while (i < indexToGet) {
+            curr = curr.next;
+            i++;
+        }
+
+        return curr.item;
     }
 
     public Iterator<Item> iterator()         // return an independent iterator over items in random order
     {
-        return new ListIterator();
+        return new ListRandomIterator(count);
     }
 
-    private class ListIterator implements Iterator<Item>
-    {
-        private Node current = first;
+    private class ListRandomIterator implements Iterator<Item> {
+        private int currIndex;
+        private int order[];
 
-        public boolean hasNext() { return current != null; }
-        public void remove() { throw new java.lang.UnsupportedOperationException(); }
-        public Item next()
+        public ListRandomIterator(int count)
         {
-            if(!hasNext()) throw new java.util.NoSuchElementException();
-            else
+            order = new int[count];
+
+            for(int i = 0; i < count; i++)
             {
-                Item item = current.item;
-                current = current.next;
-                return item;
+                order[i] = i;
             }
+            StdRandom.shuffle(order);
+            currIndex = 0;
+        }
+
+        public boolean hasNext()
+        {
+            return (currIndex < count);
+        }
+
+        public void remove() { throw new java.lang.UnsupportedOperationException(); }
+
+        public Item next() {
+            Node node = first;
+            int i = 0;
+            int index = order[currIndex];
+            while(i != index)
+            {
+                node = node.next;
+                i++;
+            }
+
+            currIndex++;
+
+            return node.item;
         }
     }
+
     public static void main(String[] args)   // unit testing (optional)
     {
+        final int testCount = 10;
 
+        RandomizedQueue<Integer> deck = new RandomizedQueue<Integer>();
+
+        System.out.println("The deck is empty: " + deck.isEmpty());
+        System.out.println("Count is: " + deck.size());
+
+        for(int i = 0; i < testCount; i++){
+            deck.enqueue(i);
+        }
+
+        System.out.println("Count is: " + deck.size() + " should be " + testCount);
+        System.out.println("The deck is NOT empty: " + !deck.isEmpty());
+
+        deck.dequeue();
+        deck.dequeue();
+
+        System.out.println("Count after removing two: " + deck.size() + " should be " + (testCount - 2));
+
+        Iterator<Integer> i  = deck.iterator();
+        while(i.hasNext()) {
+            System.out.println(i.next());
+        }
     }
 }
