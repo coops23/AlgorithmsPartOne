b340ee31dc8ac248af9ad4ddea64704d4cc7d387
Lewis Cooper <lewis.cooper@intel.com>
2019-05-23 15:34:36 -0700

    Adding assignment #5

---
+++ 8Puzzle/CreateZip.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import zipfile
+
+EXCEPTIONS = ["PuzzleChecker.java"]
+
+def zipdir(root, path, ziph, files):
+    # ziph is zipfile handle
+    for file in files:
+        ziph.write(os.path.join(root, file))
+
+if __name__ == '__main__':
+    dir = os.getcwd()
+    dir = dir.split('\\')
+    dir = dir[len(dir) - 1]
+    name = dir.lower() + "-" + "submit" + ".zip"
+    
+    items = os.listdir(".")
+    newlist = []
+    for names in items:
+        if names.endswith(".java"):
+            if(names == EXCEPTIONS[0]):
+                pass
+            else:
+                newlist.append(names)
+    print newlist
+        
+    zipf = zipfile.ZipFile(name, mode = 'w') 
+    try:
+        for file in newlist:
+            zipf.write(file)
+    finally:
+        zipf.close()
+    
